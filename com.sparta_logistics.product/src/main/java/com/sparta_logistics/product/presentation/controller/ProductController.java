db1586e0e2fb3f190844be75bc87dd74de75aacc
strongcookdas <goodjav141592@gmail.com>
2024-12-10 13:43:32 +0900

    [feat] readProduct 단건 조회 Controller, response DTO 구현 ( #3 )

--- com.sparta_logistics.product/src/main/java/com/sparta_logistics/product/presentation/controller/ProductController.java
+++ com.sparta_logistics.product/src/main/java/com/sparta_logistics/product/presentation/controller/ProductController.java
@@ -3,9 +3,13 @@ package com.sparta_logistics.product.presentation.controller;
 import com.sparta_logistics.product.application.service.ProductService;
 import com.sparta_logistics.product.presentation.dto.CreateProductRequest;
 import com.sparta_logistics.product.presentation.dto.CreateProductResponse;
+import com.sparta_logistics.product.presentation.dto.ReadProductResponse;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +27,12 @@ public class ProductController {
       @RequestBody CreateProductRequest request) {
 
     CreateProductResponse response = productService.createProduct(request);
-
     return ResponseEntity.status(HttpStatus.CREATED).body(response);
+  }
+
+  @GetMapping("/{productId}")
+  public ResponseEntity<ReadProductResponse> readProduct(@PathVariable UUID productId) {
+    ReadProductResponse response = productService.readProduct(productId);
+    return ResponseEntity.ok(response);
   }
 }
---
+++ com.sparta_logistics.product/src/main/java/com/sparta_logistics/product/presentation/dto/ReadProductResponse.java
@@ -0,0 +1,36 @@
+package com.sparta_logistics.product.presentation.dto;
+
+import com.sparta_logistics.product.domain.model.Product;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+public class ReadProductResponse {
+
+  private UUID productId;
+  private UUID companyId;
+  private UUID hubId;
+  private String productName;
+  private Integer productStock;
+  private String productImageUrl;
+  private Long productPrice;
+
+  public static ReadProductResponse of(Product product) {
+    return ReadProductResponse.builder()
+        .companyId(product.getCompanyId())
+        .hubId(product.getHubId())
+        .productId(product.getId())
+        .productName(product.getName())
+        .productImageUrl(product.getImageUrl())
+        .productStock(product.getStock())
+        .productPrice(product.getPrice())
+        .build();
+  }
+}
