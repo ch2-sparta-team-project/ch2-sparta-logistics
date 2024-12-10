baace774f5d78a02ee22a18ae8f514b5e7b3ef31
strongcookdas <goodjav141592@gmail.com>
2024-12-10 09:58:04 +0900

    [feat] ProductController, Request, Response DTO 생성 ( #3 )

---
+++ com.sparta_logistics.product/src/main/java/com/sparta_logistics/product/presentation/dto/CreateProductResponse.java
@@ -0,0 +1,16 @@
+package com.sparta_logistics.product.presentation.dto;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateProductResponse {
+
+  private UUID productId;
+}
