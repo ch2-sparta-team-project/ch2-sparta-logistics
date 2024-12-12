0770d9814287906531dc7dbc15cac0f1d5615d70
chanwookK <kan0202@naver.com>
2024-12-13 01:32:20 +0900

    [feat] AuthenticationFiler 에서 헤더 정보를 통해 유저정보 받아오도록 변경 (#8)

--- com.sparta_logistics.order/src/main/java/com/sparta_logistics/order/presentation/filter/JwtAuthenticationFilter.java
+++ com.sparta_logistics.order/src/main/java/com/sparta_logistics/order/presentation/filter/JwtAuthenticationFilter.java
@@ -1,21 +1,17 @@
 package com.sparta_logistics.order.presentation.filter;
 
+import com.sparta_logistics.order.global.exception.ApplicationException;
+import com.sparta_logistics.order.global.exception.ErrorCode;
 import com.sparta_logistics.order.presentation.dto.RequestUserDetails;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
 
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -23,23 +19,27 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
       FilterChain filterChain) throws ServletException, IOException {
-
     //Mock 데이터 생성
-    String userId = UUID.randomUUID().toString();
-    String userName = "강찬욱";
-    List<GrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority("MASTER"));
+    String userId = request.getHeader("ID_HEADER");
+    String userName = request.getHeader("NAME_HEADER");
+    String role = request.getHeader("ROLE_HEADER");
+
+    if (userId == null || userName == null || role == null) {
+      throw new ApplicationException(ErrorCode.UNAUTHORIZED_EXCEPTION);
+    }
 
     //Mock 유저 생성
-    UserDetails mockUserDetails = new RequestUserDetails(userId, userName, authorities);
+    RequestUserDetails mockUserDetails = new RequestUserDetails(userId, userName, role);
 
     // Authentication 객체 생성
     UsernamePasswordAuthenticationToken authentication =
-        new UsernamePasswordAuthenticationToken(mockUserDetails, null, authorities);
+        new UsernamePasswordAuthenticationToken(mockUserDetails, null, mockUserDetails.getAuthorities());
 
     // SecurityContext에 인증 정보 설정
     SecurityContextHolder.getContext().setAuthentication(authentication);
 
     // 필터 체인 진행
     filterChain.doFilter(request, response);
+
   }
 }
