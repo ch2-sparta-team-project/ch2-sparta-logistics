1dc6ed8ded0af7fd29e9bd5fdaa826797757649b
USER04 <jabberwocker04@gmail.com>
2024-12-13 22:48:45 +0900

    [refactor] AuthController, UserController 분리 및 GateWay Service 수정

--- com.sparta_logistics.auth/src/main/java/com/sparta_logistics/auth/Controller/AuthController.java
+++ com.sparta_logistics.auth/src/main/java/com/sparta_logistics/auth/Controller/AuthController.java
@@ -2,6 +2,7 @@ package com.sparta_logistics.auth.Controller;
 
 import com.sparta_logistics.auth.Dto.SignUpRequestDto;
 import com.sparta_logistics.auth.Service.AuthService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -10,9 +11,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,37 +24,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/auth")
 @Slf4j
 public class AuthController {
 
   private final AuthService authService;
-
-  @Getter
-  private final String serverPort;
-
-  public AuthController(AuthService authService, @Value("${server.port}") String serverPort) {
+  public AuthController(AuthService authService) {
     this.authService = authService;
-    this.serverPort = serverPort;
   }
 
-  @PostMapping("/auth/sign-up")
+  @PostMapping("/sign-up")
   public ResponseEntity<?> signUp(@RequestBody SignUpRequestDto SignUpRequestDto) {
     return ResponseEntity.ok((authService.signUp(SignUpRequestDto)));
   }
-
-  @GetMapping("/users/info")
-  public ResponseEntity<?> userInfo(@RequestHeader("Authorization") String token) {
-    String accessToken = token.replace("Bearer ", "");
-    return ResponseEntity.ok(authService.getUserInfoFromAccessToken(accessToken));
-  }
-
-  @DeleteMapping("/users/delete")
-  public ResponseEntity<?> softDeleteUser(@RequestHeader("Authorization") String token) {
-    String accessToken = token.replace("Bearer ", "");
-    authService.softDeleteUser(accessToken);
-    return ResponseEntity.ok("회원 삭제 완료");
-  }
-
 
 }
