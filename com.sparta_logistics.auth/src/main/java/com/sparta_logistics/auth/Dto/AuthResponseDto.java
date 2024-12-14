7f821b3badab6b60d50e04b756e4c1d9e5e2b264
USER04 <jabberwocker04@gmail.com>
2024-12-14 20:40:15 +0900

    [feature] User 기능의 응답용 ResponseDto 생성

---
+++ com.sparta_logistics.auth/src/main/java/com/sparta_logistics/auth/Dto/AuthResponseDto.java
@@ -0,0 +1,28 @@
+package com.sparta_logistics.auth.Dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AuthResponseDto {
+    private String message;
+    private Integer statusCode;
+    private Object data;
+
+    public AuthResponseDto(String message, Integer statusCode, Object data) {
+      this.message = message;
+      this.statusCode = statusCode;
+      this.data = data;
+    }
+
+  public AuthResponseDto(String message, Integer statusCode) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+
+}
