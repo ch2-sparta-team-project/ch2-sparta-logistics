530f3669eedd9cd448a3b4b89b61c2d23f5ef51e
yshong1998 <yshong1998@gmail.com>
2024-12-17 18:28:00 +0900

    [feat] Delivery 도메인에 Swagger 적용

--- com.sparta_logistics.delivery/src/main/java/com/sparta_logistics/delivery/presentation/filter/JwtAuthenticationFilter.java
+++ com.sparta_logistics.delivery/src/main/java/com/sparta_logistics/delivery/presentation/filter/JwtAuthenticationFilter.java
@@ -18,27 +18,32 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
       FilterChain filterChain) throws ServletException, IOException {
-    //Mock 데이터 생성
-    String userId = request.getHeader("X-User-Id");
-    String userName = request.getHeader("X-User-Name");
-    String role = request.getHeader("X-User-Role");
 
-    if (userId == null || userName == null || role == null) {
-      throw new ApplicationException(ErrorCode.UNAUTHORIZED_EXCEPTION);
+    if (request.getRequestURI().startsWith("/api/v1/deliveries/v3/api-docs") ||
+        request.getRequestURI().startsWith("/api/v1/deliveries/swagger")){
+      filterChain.doFilter(request, response);
+    } else {
+      //Mock 데이터 생성
+      String userId = request.getHeader("X-User-Id");
+      String userName = request.getHeader("X-User-Name");
+      String role = request.getHeader("X-User-Role");
+
+      if (userId == null || userName == null || role == null) {
+        throw new ApplicationException(ErrorCode.UNAUTHORIZED_EXCEPTION);
+      }
+
+      //Mock 유저 생성
+      RequestUserDetails mockUserDetails = new RequestUserDetails(userId, userName, role);
+
+      // Authentication 객체 생성
+      UsernamePasswordAuthenticationToken authentication =
+          new UsernamePasswordAuthenticationToken(mockUserDetails, null, mockUserDetails.getAuthorities());
+
+      // SecurityContext에 인증 정보 설정
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+
+      // 필터 체인 진행
+      filterChain.doFilter(request, response);
     }
-
-    //Mock 유저 생성
-    RequestUserDetails mockUserDetails = new RequestUserDetails(userId, userName, role);
-
-    // Authentication 객체 생성
-    UsernamePasswordAuthenticationToken authentication =
-        new UsernamePasswordAuthenticationToken(mockUserDetails, null, mockUserDetails.getAuthorities());
-
-    // SecurityContext에 인증 정보 설정
-    SecurityContextHolder.getContext().setAuthentication(authentication);
-
-    // 필터 체인 진행
-    filterChain.doFilter(request, response);
-
   }
 }
