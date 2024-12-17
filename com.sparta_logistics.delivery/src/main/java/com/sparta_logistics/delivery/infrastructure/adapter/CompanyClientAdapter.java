4019ab154033dea38157680ae01c60f7da28db23
chanwookK <kan0202@naver.com>
2024-12-17 10:46:36 +0900

    [refactor] Delivery Service 의 Company Client 연결  (#63)

--- com.sparta_logistics.delivery/src/main/java/com/sparta_logistics/delivery/infrastructure/adapter/CompanyClientAdapter.java
+++ com.sparta_logistics.delivery/src/main/java/com/sparta_logistics/delivery/infrastructure/adapter/CompanyClientAdapter.java
@@ -1,15 +1,27 @@
 package com.sparta_logistics.delivery.infrastructure.adapter;
 
 import com.sparta_logistics.delivery.application.port.CompanyClientPort;
+import com.sparta_logistics.delivery.infrastructure.client.CompanyClient;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class CompanyClientAdapter implements CompanyClientPort {
+
+  private final CompanyClient companyClient;
+
   @Override
   public String findCompanyAffiliationHubIdByUserId(String userId) {
-    return "mock_affiliation_hub_id";
+    return companyClient.searchCompanies(
+            null, UUID.fromString(userId),
+            Pageable.unpaged(),
+            "ROLE_MASTER","85460f92-558c-4efa-84de-7607dfd941b3","123"
+        ).getBody().getContent()
+        .get(0)
+        .getHubId().toString();
   }
 
 }
